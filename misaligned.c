ada3be5b38896d20c9f3ff7b5d2ba4d1ce578f14
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-08-11 18:48:39 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -7,7 +7,7 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
@@ -16,6 +16,8 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(result == 16);
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
